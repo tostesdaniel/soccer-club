99b29520bea7496b6f7e6f0fa6ba6c2643f960a7
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-28 19:59:07 -0300

    fix(users): change user table name

--- app/backend/src/database/migrations/1-create-users.js
+++ app/backend/src/database/migrations/1-create-users.js
@@ -1,6 +1,6 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable("Users", {
+    await queryInterface.createTable("users", {
       id: {
         allowNull: false,
         autoIncrement: true,
