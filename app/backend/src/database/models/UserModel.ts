2bdf649f0637c33963a64ea68fbb243a35ad7df0
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-28 19:12:21 -0300

    test(UserModel): create UserModel

---
+++ app/backend/src/database/models/UserModel.ts
@@ -0,0 +1,43 @@
+import { DataTypes, Model } from 'sequelize';
+import db from '.';
+
+class User extends Model {
+  id!: number;
+  username!: string;
+  role!: string;
+  email!: string;
+  password!: string;
+}
+
+User.init(
+  {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+    },
+    username: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    role: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  },
+  {
+    underscored: true,
+    sequelize: db,
+    timestamps: false,
+  },
+);
+
+export default User;
