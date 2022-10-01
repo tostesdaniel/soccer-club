b961cfa6fffc0ba06449186ec84f9af34f062d68
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-01 16:42:40 -0300

    feat: add password verification on login

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -1,9 +1,12 @@
 import { ValidationError } from 'joi';
+import * as jwt from 'jsonwebtoken';
 import IJwtToken from '../interfaces/JwtToken.interface';
 import ILogin from '../interfaces/Login.interface';
 import User from '../database/models/UserModel';
 import generateJWT from '../utils/auth/generateJWT';
 import loginSchema from '../validations/login.schema';
+import validatePassword from '../utils/validatePassword';
+import { IUserPayload } from '../interfaces/User.interface';
 
 export default class UserService {
   constructor(private _userModel: typeof User) {}
@@ -15,12 +18,18 @@ export default class UserService {
     const instance = await this._userModel.findOne({
       where: { email: user.email },
     });
-    if (!instance) {
+    if (!instance || !validatePassword(user.password, instance.password)) {
       throw new Error('401|Incorrect email or password');
     }
 
     const token = generateJWT(instance.id, instance.role);
 
     return token;
+  }
+
+  static async validateLogin(token: string) {
+    const { role } = jwt.decode(token) as IUserPayload;
+
+    return { role };
   }
 }
