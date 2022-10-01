dd300194060f471312e722cdecc5e124608b75c5
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-01 19:04:39 -0300

    feat: add teams mocks to index

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -1,4 +1,5 @@
 import userMocks from './user.mock';
 import tokenMock from './token.mock';
+import teamMock from './team.mock';
 
-export default { userMocks, tokenMock };
+export default { userMocks, tokenMock, teamMock };
