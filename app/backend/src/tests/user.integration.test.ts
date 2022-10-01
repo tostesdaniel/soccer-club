43a02860a9a68f49a5db326d11df123d581b3ad0
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-01 16:45:23 -0300

    test: change resolved value for admin password

--- app/backend/src/tests/user.integration.test.ts
+++ app/backend/src/tests/user.integration.test.ts
@@ -18,7 +18,8 @@ describe('Rota de login', () => {
         username: 'Admin',
         role: 'admin',
         email: 'admin@admin.com',
-        password: 'secret_admin',
+        password:
+          '$2a$08$xi.Hxk1czAO0nZR..B393u10aED0RQ1N3PAEXQ7HxtLjKPEZBu.PW',
       } as User);
     });
 
