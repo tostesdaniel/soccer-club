5a5b6e7847758cebb83ac4b52f741a69559a20a1
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-01 18:08:51 -0300

    feat: create user token mocks

---
+++ app/backend/src/tests/mocks/token.mock.ts
@@ -0,0 +1,6 @@
+export default {
+  adminToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwicm9sZSI6ImFkbWluIiwiaWF0IjoxNjY0NjU0MjYzfQ._T_BrQ3e98ZgJC3y-gkRfVfTlu1qo-wiWk3OyhAv0VM',
+  userToken:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Miwicm9sZSI6InVzZXIiLCJpYXQiOjE2NjQ2NTQ4MTJ9.wetnNduXRWH7L_XaTfePwfj3WkN46axjcwNiRar_jg8',
+};
