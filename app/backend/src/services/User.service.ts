dfc3e7ff250702f3ebd1d2ca4e9090ed82de57bf
Daniel Tostes <ohtosttnz@gmail.com>
2022-09-30 23:47:31 -0300

    feat: login now accepts Joi errors

--- app/backend/src/services/User.service.ts
+++ app/backend/src/services/User.service.ts
@@ -1,12 +1,17 @@
+import { ValidationError } from 'joi';
 import IJwtToken from '../interfaces/JwtToken.interface';
 import ILogin from '../interfaces/Login.interface';
 import User from '../database/models/UserModel';
 import generateJWT from '../utils/auth/generateJWT';
+import loginSchema from '../validations/login.schema';
 
 export default class UserService {
   constructor(private _userModel: typeof User) {}
 
-  async login(user: ILogin): Promise<IJwtToken> {
+  async login(user: ILogin): Promise<IJwtToken | ValidationError> {
+    const { error } = loginSchema.login.validate(user);
+    if (error) throw new Error(error.details[0].message);
+
     const instance = await this._userModel.findOne({
       where: { email: user.email },
     });
