5840f9b7285cea21608051378c7c2b2ac34a8834
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-03 19:39:37 -0300

    feat: implement get all matches

---
+++ app/backend/src/services/Match.service.ts
@@ -0,0 +1,17 @@
+import Match from '../database/models/MatchModel';
+import Team from '../database/models/TeamModel';
+
+export default class MatchService {
+  constructor(private _matchModel: typeof Match) {}
+
+  async getAll(): Promise<Match[]> {
+    const matches = await this._matchModel.findAll({
+      include: [
+        { model: Team, attributes: { exclude: ['id'] }, as: 'teamHome' },
+        { model: Team, attributes: { exclude: ['id'] }, as: 'teamAway' },
+      ],
+    });
+
+    return matches;
+  }
+}
