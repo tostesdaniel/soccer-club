fce381c0446dd83b8e3d8745a72b0169ecc8609f
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-03 20:22:48 -0300

    feat: implement get matches by progress

--- app/backend/src/controllers/Match.controller.ts
+++ app/backend/src/controllers/Match.controller.ts
@@ -9,4 +9,14 @@ export default class MatchController {
 
     return res.status(200).json(matches);
   }
+
+  async getMatchesByProgress(req: Request, res: Response): Promise<Response> {
+    const { inProgress } = req.query;
+
+    const matches = await this._matchService.getMatchesByProgress(
+      Boolean(inProgress),
+    );
+
+    return res.status(200).json(matches);
+  }
 }
