41d6a7ea7ad0ca46857facffedd5782ad1bd6cb5
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-03 19:40:34 -0300

    feat: add matchRouter to index

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,4 +1,5 @@
 import loginRouter from './login.route';
 import teamRouter from './team.route';
+import matchRouter from './match.route';
 
-export default { loginRouter, teamRouter };
+export default { loginRouter, teamRouter, matchRouter };
