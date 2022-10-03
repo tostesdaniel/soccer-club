ffcb5a3513c164c660e8a19cfd3ac5bf91508787
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-03 15:37:15 -0300

    feat: add matchesMock to index

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -1,5 +1,6 @@
 import userMocks from './user.mock';
 import tokenMock from './token.mock';
 import teamMock from './team.mock';
+import matchesMock from './matches.mock';
 
-export default { userMocks, tokenMock, teamMock };
+export default { userMocks, tokenMock, teamMock, matchesMock };
