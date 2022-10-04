d4eeedd0b71d3f24f25a61abbb96900599c62de7
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-04 10:45:55 -0300

    feat: implement create match method and insertion validation

--- app/backend/src/services/Match.service.ts
+++ app/backend/src/services/Match.service.ts
@@ -4,6 +4,22 @@ import Team from '../database/models/TeamModel';
 export default class MatchService {
   constructor(private _matchModel: typeof Match) {}
 
+  static async verifyCreation({ homeTeam, awayTeam }: Match) {
+    const sameTeamOnBothSides = homeTeam === awayTeam;
+    if (sameTeamOnBothSides) {
+      throw new Error(
+        '401|It is not possible to create a match with two equal teams',
+      );
+    }
+
+    const existingTeams = await Team.findAll({
+      where: { id: [homeTeam, awayTeam] },
+    });
+    if (existingTeams.length !== 2) {
+      throw new Error('404|There is no team with such id!');
+    }
+  }
+
   async getAll(): Promise<Match[]> {
     const matches = await this._matchModel.findAll({
       include: [
@@ -25,5 +41,13 @@ export default class MatchService {
     });
 
     return matches;
+  }
+
+  async create(match: Match): Promise<Match> {
+    await MatchService.verifyCreation(match);
+
+    const instance = await this._matchModel.create(match);
+
+    return instance;
   }
 }
