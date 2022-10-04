71ba7176a4e8b3ded9d94a7a1d37905a6b20cbd3
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-04 12:13:40 -0300

    feat: add leaderboard mocks to mock index

--- app/backend/src/tests/mocks/index.ts
+++ app/backend/src/tests/mocks/index.ts
@@ -2,5 +2,6 @@ import userMocks from './user.mock';
 import tokenMock from './token.mock';
 import teamMock from './team.mock';
 import matchesMock from './matches.mock';
+import leaderboardMock from './leaderboard.mock';
 
-export default { userMocks, tokenMock, teamMock, matchesMock };
+export default { userMocks, tokenMock, teamMock, matchesMock, leaderboardMock };
