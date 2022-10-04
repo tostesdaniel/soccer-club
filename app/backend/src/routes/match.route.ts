ce49de50a969bd3613ddd8b4058eb168d43e3176
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-04 10:46:56 -0300

    feat: implement POST /matches route

--- app/backend/src/routes/match.route.ts
+++ app/backend/src/routes/match.route.ts
@@ -2,11 +2,14 @@ import * as express from 'express';
 import Match from '../database/models/MatchModel';
 import MatchService from '../services/Match.service';
 import MatchController from '../controllers/Match.controller';
+import validateJWT from '../middlewares/auth/validateJWT';
 
 require('express-async-errors');
 
 const match = new MatchController(new MatchService(Match));
 const matchRouter = express.Router();
+
+matchRouter.post('/', validateJWT, (req, res) => match.create(req, res));
 
 matchRouter.get('/', (req, res) => match.getAll(req, res));
 
