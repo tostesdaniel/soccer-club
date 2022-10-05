241aedb79fa2df2ae6b6680880c317ac327deb79
Daniel Tostes <ohtosttnz@gmail.com>
2022-10-04 21:37:40 -0300

    feat: add leaderboard router to router index

--- app/backend/src/routes/index.ts
+++ app/backend/src/routes/index.ts
@@ -1,5 +1,6 @@
 import loginRouter from './login.route';
 import teamRouter from './team.route';
 import matchRouter from './match.route';
+import leaderboardRouter from './leaderboard.route';
 
-export default { loginRouter, teamRouter, matchRouter };
+export default { loginRouter, teamRouter, matchRouter, leaderboardRouter };
