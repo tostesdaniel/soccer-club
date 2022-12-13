3760554332d7964037415e182103715fb8cd5a6e
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-12 22:09:45 -0300

    fix: add https:// to axios baseURL

--- app/frontend/src/services/requests.js
+++ app/frontend/src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: `soccer-club-production.up.railway.app`,
+  baseURL: `https://soccer-club-production.up.railway.app`,
 });
 
 export const setToken = (token) => {
