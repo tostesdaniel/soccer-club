0b0f110be99fb541cb20cd31c77ba5dea69a5044
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-16 23:18:49 -0300

    chore: update prettier config

--- app/frontend/prettier.config.js
+++ app/frontend/prettier.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  plugins: [require("prettier-plugin-tailwindcss")],
-};
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tabWidth: 2,
+  semi: false,
+  jsxSingleQuote: false,
+  singleQuote: true,
+}
