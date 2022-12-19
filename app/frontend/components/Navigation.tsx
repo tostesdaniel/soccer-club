97d39ded110ba4a073c51f9534a3f9aad24aa6b9
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-18 21:20:25 -0300

    feat: create navigation bar for desktop and mobile

---
+++ app/frontend/components/Navigation.tsx
@@ -0,0 +1,94 @@
+import { Disclosure } from '@headlessui/react'
+import { UsersIcon } from '@heroicons/react/20/solid'
+import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+
+const navigation = [
+  { name: 'Times', href: '/matches', current: true },
+  { name: 'Partidas', href: '/leaderboard', current: false },
+]
+
+export default function Navigation() {
+  const router = useRouter()
+
+  return (
+    <Disclosure as="nav" className="bg-gray-800">
+      {({ open }) => (
+        <>
+          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 justify-between">
+              <div className="flex">
+                <div className="-ml-2 mr-2 flex items-center md:hidden">
+                  <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                    <span className="sr-only">Abrir menu</span>
+                    {open ? (
+                      <XMarkIcon
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      ></XMarkIcon>
+                    ) : (
+                      <Bars3Icon
+                        className="h-6 w-6"
+                        aria-hidden="true"
+                      ></Bars3Icon>
+                    )}
+                  </Disclosure.Button>
+                </div>
+                <div className="hidden md:ml-6 md:flex md:items-center md:space-x-4">
+                  {navigation.map((item) => (
+                    <Link
+                      key={item.name}
+                      href={item.href}
+                      className={`rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white ${
+                        item.current ? 'text-white hover:bg-gray-900' : null
+                      }`}
+                      aria-current={item.current ? 'page' : undefined}
+                    >
+                      {item.name}
+                    </Link>
+                  ))}
+                </div>
+              </div>
+              <div className="flex items-center">
+                <div className="flex-shrink-0">
+                  <button
+                    type="button"
+                    className="relative inline-flex items-center rounded-md border border-transparent bg-indigo-500 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-800"
+                    onClick={() => router.push('/login')}
+                  >
+                    <UsersIcon
+                      className="mr-2 -ml-1 h-5 w-5"
+                      aria-hidden="true"
+                    />
+                    <span>Login</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="md:hidden">
+            <div className="space-y-1 px-2 pt-2 pb-3 sm:px-3">
+              {navigation.map((item) => (
+                <Disclosure.Button
+                  key={item.name}
+                  as={Link}
+                  href={item.href}
+                  className={`block rounded-md px-3 py-2 text-base font-medium ${
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white'
+                  }`}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </Disclosure.Button>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
