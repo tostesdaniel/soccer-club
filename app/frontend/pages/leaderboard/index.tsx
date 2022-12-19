7dca53a0a1a9c9c54a8a01c650e46583a39f1b24
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-19 20:18:59 -0300

    style: add margin bottom

--- app/frontend/pages/leaderboard/index.tsx
+++ app/frontend/pages/leaderboard/index.tsx
@@ -26,7 +26,7 @@ export default function Leaderboard() {
   }, [])
 
   return (
-    <div className="mt-10 px-4 sm:px-6 lg:px-8">
+    <div className="mt-10 mb-10 px-4 sm:px-6 lg:px-8">
       <div>
         <div>
           <h1 className="text-xl font-semibold text-gray-900">Classificação</h1>
