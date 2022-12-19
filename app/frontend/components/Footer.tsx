6432736b175a9ca8b648bcb65e76e49f92691b0a
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-19 15:46:42 -0300

    style: margin top auto to stick footer to bottom

--- app/frontend/components/Footer.tsx
+++ app/frontend/components/Footer.tsx
@@ -44,7 +44,7 @@ const navigation = [
 
 export default function Footer() {
   return (
-    <footer className="bg-gray-800">
+    <footer className="mt-auto bg-gray-800">
       <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 md:flex md:items-center md:justify-between lg:px-8">
         <div className="flex justify-center space-x-6 md:order-2">
           {navigation.map((item) => (
