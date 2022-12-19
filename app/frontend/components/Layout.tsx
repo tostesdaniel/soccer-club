fdc3cb60529831c93c6547f674402af80fe33934
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-19 19:59:24 -0300

    style: set main flex-grow: 1 so it wont center

--- app/frontend/components/Layout.tsx
+++ app/frontend/components/Layout.tsx
@@ -10,7 +10,7 @@ export default function Layout({ children }: Props) {
   return (
     <>
       <Navigation />
-      <main className="h-full">{children}</main>
+      <main className="h-full grow">{children}</main>
       <Footer />
     </>
   )
