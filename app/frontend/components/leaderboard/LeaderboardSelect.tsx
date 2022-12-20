cd119af494c59c4325c8294c86de8cf52606dde8
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-20 19:57:42 -0300

    feat: create styled options box with unstyled child options

--- app/frontend/components/leaderboard/LeaderboardSelect.tsx
+++ app/frontend/components/leaderboard/LeaderboardSelect.tsx
@@ -1,4 +1,4 @@
-import { Listbox } from '@headlessui/react'
+import { Listbox, Transition } from '@headlessui/react'
 import { ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import { useState } from 'react'
 
@@ -18,7 +18,7 @@ export default function LeaderboardSelect() {
           <Listbox.Label className="block text-sm font-medium text-gray-700">
             Exibir times
           </Listbox.Label>
-          <div className="mt-1">
+          <div className="relative mt-1">
             <Listbox.Button className="relative w-full cursor-default rounded-md border border-gray-300 bg-white py-2 pl-3 pr-10 text-left shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-1 focus:ring-indigo-500 sm:text-sm">
               <span className="block truncate">{selected.name}</span>
               <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
@@ -28,6 +28,16 @@ export default function LeaderboardSelect() {
                 />
               </span>
             </Listbox.Button>
+
+            <Transition>
+              <Listbox.Options className="absolute z-10 mt-1 max-h-60 w-full rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+                {leaderboardTypes.map(({ name, path }) => (
+                  <Listbox.Option key={name} value={path}>
+                    {name}
+                  </Listbox.Option>
+                ))}
+              </Listbox.Options>
+            </Transition>
           </div>
         </>
       )}
