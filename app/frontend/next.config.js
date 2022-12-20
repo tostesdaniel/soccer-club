08aa58274b4a33fb73d22f16a45a2e1808f3e800
Daniel Tostes <ohtosttnz@gmail.com>
2022-12-20 12:47:25 -0300

    chore: redirect to `/leaderboard` from `/`

--- app/frontend/next.config.js
+++ app/frontend/next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  redirects: async () => {
+    return [
+      {
+        source: '/',
+        destination: '/leaderboard',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
